d19ee54c90c5b4ae95b32f890c9f43c0c1ca932e
Ismael da Silva Lourenço <75037197+IsmaLourenco@users.noreply.github.com>
2022-03-17 21:59:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function ({
+
+    const form = document.querySelector('form');
+    const input = document.querySelector('input');
+
+    form.addEventListener('submit', (event) => {
+event.preventDefault();
+    });
+
+})
