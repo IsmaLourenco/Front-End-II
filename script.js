d62126a0dc7b00fe2f6068fbcc0d9df97e7c36fc
glottocardoso <glottocardoso@gmail.com>
2022-03-20 15:04:38 -0300

    Melhorando Script.js

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 window.onload = function(){
     const form = document.querySelector('form');
-    form.addEventListener('submit', function(event)
-{
-event.preventDefault();
+    form.addEventListener('submit', function(event){
+    event.preventDefault();
 
-const cartao = document.createElement('div');
-const img = document.createElement('img');
-const h4 = document.createElement('h4');
-const b = document.createElement('b');
-const p = document.createElement('p');
+    //Criando elementos:
+    const cartao = document.createElement('div');
+    const img = document.createElement('img');
+    const h4 = document.createElement('h4');
+    const b = document.createElement('b');
+    const p = document.createElement('p');
 
+    //Adquirindo dados:
+    const chassiValor = document.querySelector('input#chassis').value
+    const modeloValor = document.querySelector('select#modelo').value
+    const imgLink = document.querySelector('input#imagem').value
+    const defeito = document.querySelector('textarea').value
 
-const chassiValor = document.querySelector('input#chassis').value
-const imgLink = document.querySelector('input#imagem').value
-const defeito = document.querySelector('textarea').value
+    //Adicionando imagem:
+    img.setAttribute('src',imgLink);
 
+    //Adicionando textos:
+    const btexto = document.createTextNode(`${modeloValor} - ${chassiValor}`);
+    b.appendChild(btexto);
+    const ptext =document.createTextNode(defeito);
+    p.appendChild(ptext);
 
-img.setAttribute('src',imgLink);
+    //Adicionando classes:
+    cartao.classList.add('cartao');
+    img.classList.add('cartao-img');
+    p.classList.add('cartao-texto');
+    h4.classList.add('cartao-titulo');
 
-const btexto = document.createTextNode(chassiValor);
-b.appendChild(btexto);
+    //Append dos elementos:
+    h4.appendChild(b);
+    cartao.appendChild(img);
+    cartao.appendChild(h4);
+    cartao.appendChild(p);
 
-const ptext =document.createTextNode(defeito);
-p.appendChild(ptext);
-
-
-cartao.classList.add('cartao');
-img.classList.add('cartao-img');
-
-
-
-p.classList.add('cartao-texto');
-
-
-h4.appendChild(b);
-cartao.appendChild(img);
-cartao.appendChild(h4);
-cartao.appendChild(p);
-
-document.querySelector('.cartoes').appendChild(cartao);
-form.reset();
-});
-
+    document.querySelector('.cartoes').appendChild(cartao);
+    form.reset();
+    });
 }
